cd0f6357b946532da3929b7a25f53807f02c5714
Charlotte <1515774621@qq.com>
2017-02-18 01:48:54 +0800

    second commit

--- app/src/main/java/com/charlotte/mynamiapplication/MainActivity.java
+++ app/src/main/java/com/charlotte/mynamiapplication/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Toast mToast=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,47 +18,34 @@ public class MainActivity extends AppCompatActivity {
         // 标题的文字需在setSupportActionBar之前，不然会无效
         mToolbar.setTitle("我的应用作品集");
         setSupportActionBar(mToolbar);
+    }
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        Button button6 = (Button) findViewById(R.id.button6);
+    public void btnClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                show(R.string.toast_button2);
+                break;
+            case R.id.button3:
+                show(R.string.toast_button3);
+                break;
+            case R.id.button4:
+                show(R.string.toast_button4);
+                break;
+            case R.id.button5:
+                show(R.string.toast_button5);
+                break;
+            case R.id.button6:
+                show(R.string.toast_button6);
+                break;
+        }
+    }
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "启动应用:热门电影", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "启动应用:股票雄鹰", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "启动应用:XYZ阅读器", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "启动应用:最新闻", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "启动应用:毕业设计", Toast.LENGTH_LONG).show();
-            }
-        });
-
+    private void show(int resId){
+        if(mToast==null){
+            mToast=Toast.makeText(this,resId,Toast.LENGTH_SHORT);
+        }else{
+            mToast.setText(resId);
+        }
+        mToast.show();
     }
 }
